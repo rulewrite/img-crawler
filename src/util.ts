7c420e627cc544a1b4fca2b71929423608502085
rulewrite <rulewrite@gmail.com>
2021-08-13 00:46:16 +0900

    feat: html 가져오기

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,13 @@
+import axios from 'axios';
+
 export const getArguments = () => {
   return process.argv.slice(2);
+};
+
+export const getHtml = async (url: string) => {
+  try {
+    return (await axios.get<string>(url)).data;
+  } catch (error) {
+    return '';
+  }
 };
--- test/util.test.ts
+++ test/util.test.ts
@@ -1,8 +1,24 @@
-import { getArguments } from '../src/util';
+import axios from 'axios';
+import { getArguments, getHtml } from '../src/util';
+
+jest.mock('axios');
+
+beforeEach(() => {
+  jest.resetAllMocks();
+});
 
 test('node 인자 가져오기', () => {
   const expected = ['arg1', 'arg2'];
   process.argv = ['execute node path', 'execute js file', ...expected];
 
   expect(getArguments()).toEqual(expect.arrayContaining(expected));
+});
+
+test('html 가져오기', async () => {
+  const data = '<html></html>';
+
+  axios.get = jest.fn().mockResolvedValue({ data });
+  const response = await getHtml('');
+
+  expect(response).toEqual(data);
 });
