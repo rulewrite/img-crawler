ee0e88cefc5d53a12a4f66f502cadef41b1c567d
rulewrite <rulewrite@gmail.com>
2021-08-28 15:53:53 +0900

    feat: 페이지 순회 로직 추가

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,26 @@ import {
   getContents,
   convertAbsoluteUrls,
   getFilename,
+  getTime,
 } from './util';
 import * as fs from 'node:fs';
 import axios from 'axios';
 
 (async () => {
-  const [url, selector] = getArguments();
+  const [url, selector, start = '1', end = 'Infinity'] = getArguments();
+  const startNumber = Number(start);
+  const endNumber = Number(end);
+
+  if (Number.isNaN(endNumber) || Number.isNaN(startNumber)) {
+    console.error('페이지 번호가 올바르지 않습니다.');
+    return;
+  }
+
+  if (endNumber < startNumber) {
+    console.error('종료 번호가 시작 번호보다 작을 수 없습니다.');
+    return;
+  }
+
   if (!url) {
     console.error('url이 없습니다.');
     return;
@@ -20,40 +34,51 @@ import axios from 'axios';
     return;
   }
 
-  const { title, html } = await getContents(url);
-  if (!html.length) {
-    console.error('응답받은 컨텐츠가 없습니다. url을 다시 확인해주세요.');
-    return;
-  }
+  let rootDirectory = '';
+  for (let index = startNumber; index <= endNumber; index++) {
+    const currentUrl = url.replaceAll('{replace}', String(index));
+    const { title, html } = await getContents(currentUrl);
 
-  const elements = getElements(html, selector);
-  if (!elements.length) {
-    console.error('엘리먼트를 찾지 못했습니다. selector를 다시 확인해주세요.');
-    return;
-  }
+    if (!html.length) {
+      console.error('응답받은 컨텐츠가 없습니다. url을 다시 확인해주세요.');
+      return;
+    }
 
-  const urlInstance = new URL(url);
-  const srcs = elements
-    .map((element) => element.attr('src'))
-    .map((src) => convertAbsoluteUrls(src, urlInstance));
-
-  const directory = `./${title}`;
-  if (!fs.existsSync(directory)) {
-    fs.mkdirSync(directory);
-  }
-
-  await Promise.all(
-    srcs.map(async (src, index) => {
-      const response = await axios.get(src, {
-        responseType: 'arraybuffer',
-      });
-
-      fs.writeFileSync(
-        `${directory}/${index + 1}-${getFilename(src)}`,
-        response.data
+    const elements = getElements(html, selector);
+    if (!elements.length) {
+      console.error(
+        '엘리먼트를 찾지 못했습니다. selector를 다시 확인해주세요.'
       );
-    })
-  );
+      return;
+    }
+
+    const urlInstance = new URL(currentUrl);
+    const srcs = elements
+      .map((element) => element.attr('src'))
+      .map((src) => convertAbsoluteUrls(src, urlInstance));
+
+    if (index === startNumber) {
+      rootDirectory = `./${title}-${getTime()}`;
+    }
+
+    const directory = `${rootDirectory}/${index}-${title}`;
+    if (!fs.existsSync(directory)) {
+      fs.mkdirSync(directory, { recursive: true });
+    }
+
+    await Promise.all(
+      srcs.map(async (src, index) => {
+        const response = await axios.get(src, {
+          responseType: 'arraybuffer',
+        });
+
+        fs.writeFileSync(
+          `${directory}/${index + 1}-${getFilename(src)}`,
+          response.data
+        );
+      })
+    );
+  }
 })().finally(() => {
   process.exit();
 });
