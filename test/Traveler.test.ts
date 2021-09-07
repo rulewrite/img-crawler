19f2249a1d8999343ec670cc26d04e37d0758fc2
rulewrite <rulewrite@gmail.com>
2021-09-07 22:40:38 +0900

    chore: 테스트 파일 약간의 코드 정리

--- test/Traveler.test.ts
+++ test/Traveler.test.ts
@@ -1,42 +1,40 @@
 import Traveler from '../src/Traveler';
 
-describe('컨텐츠 받아오기', () => {
-  let traveler: Traveler;
-  beforeAll(async () => {
-    traveler = new Traveler();
-    await traveler.launch();
+let traveler: Traveler;
+beforeAll(async () => {
+  traveler = new Traveler();
+  await traveler.launch();
+});
+
+const validUrl = 'https://google.com';
+
+describe('성공', () => {
+  test('구글 html 로드', async () => {
+    const { title } = await traveler.goto(validUrl);
+    expect(Boolean(title.length)).toEqual(true);
+  });
+});
+
+describe('실패', () => {
+  const notValidUrlSegment = `dummy-${Date.now()}`;
+  const defaultContents = {
+    title: '',
+    html: '',
+  };
+
+  test('존재하지 않는 사이트', async () => {
+    await expect(
+      traveler.goto(`https://${notValidUrlSegment}.com`)
+    ).resolves.toEqual(defaultContents);
   });
 
-  const validUrl = 'https://google.com';
-
-  describe('성공', () => {
-    test('구글 html 로드', async () => {
-      const { title } = await traveler.goto(validUrl);
-      expect(Boolean(title.length)).toEqual(true);
-    });
+  test('존재하지 않는 페이지', async () => {
+    await expect(
+      traveler.goto(`${validUrl}/${notValidUrlSegment}`)
+    ).resolves.toEqual(defaultContents);
   });
+});
 
-  describe('실패', () => {
-    const notValidUrlSegment = `dummy-${Date.now()}`;
-    const defaultContents = {
-      title: '',
-      html: '',
-    };
-
-    test('존재하지 않는 사이트', async () => {
-      await expect(
-        traveler.goto(`https://${notValidUrlSegment}.com`)
-      ).resolves.toEqual(defaultContents);
-    });
-
-    test('존재하지 않는 페이지', async () => {
-      await expect(
-        traveler.goto(`${validUrl}/${notValidUrlSegment}`)
-      ).resolves.toEqual(defaultContents);
-    });
-  });
-
-  afterAll(async () => {
-    await traveler.close();
-  });
+afterAll(async () => {
+  await traveler.close();
 });
